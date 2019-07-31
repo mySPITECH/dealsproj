25643d45fc9df93de992969859668572ab529eff
unknown <samuelsamafolabi@gmail.com>
2019-07-31 01:17:54 +0100

    Added files and changes required for deployment to heroku

--- appl/settings.py
+++ appl/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 AVARTA=os.path.join(BASE_DIR, 'media/avarta.jpg')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #Email Alert Parameters
 EMAIL_HOST = "smtp.gmail.com"#"mocha3023.mochahost.com"
 EMAIL_PORT ="587"
